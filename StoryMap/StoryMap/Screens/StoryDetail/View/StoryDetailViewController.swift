4df340157dcb60a0531e95a8a60d66816e56ca6b
felixboehm <kontakt@felixboehm.it>
2021-11-10 20:56:29 -0100

    remove tag textfield

--- StoryMap/StoryMap/Screens/StoryDetail/View/StoryDetailViewController.swift
+++ StoryMap/StoryMap/Screens/StoryDetail/View/StoryDetailViewController.swift
@@ -12,7 +12,6 @@ class StoryDetailViewController: UIViewController {
     private var viewModel: StoryDetailViewModelType
 
     private let imageView = UIImageView()
-    private var tagField = UITextField()
     
     init(viewModel: StoryDetailViewModelType) {
         self.viewModel = viewModel
@@ -27,12 +26,6 @@ class StoryDetailViewController: UIViewController {
         title = viewModel.story.title
         view.backgroundColor = .white
         setupImageView()
-//        setupMapView()
-//        if owner = "me" { setupTagLabel() } else
-//        setupTagTextField()
-//        setupPrivateToggle()
-//        setupAddStoryBook()
-//        How to change title if title is in NavigationBar?
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -50,20 +43,6 @@ class StoryDetailViewController: UIViewController {
             make.top.equalTo(view.safeAreaLayoutGuide.snp.top).offset(StyleKit.metrics.padding.verySmall)
             make.width.height.lessThanOrEqualTo(view.safeAreaLayoutGuide.snp.width)
             make.centerX.equalToSuperview()
-        }
-    }
-    
-    private func setupTagTextField() {
-        StyleKit.styleTextField(&tagField)
-//        tagField.delegate = self
-        
-        tagField.text = "#lulu, #waterfall, #azores"
-        
-        view.addSubview(tagField)
-        
-        tagField.snp.makeConstraints { make in
-            make.top.equalTo(imageView.snp.bottom).offset(StyleKit.metrics.padding.large)
-            make.leading.trailing.equalToSuperview().inset(StyleKit.metrics.padding.common)
         }
     }
 }
