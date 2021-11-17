6b4f4ef5ea3f9d7cc6df0eb364728d586f8ac174
Dorota Piačeková <dory.piacek@gmail.com>
2021-11-17 14:27:21 +0100

    Added logger

--- StoryMap/StoryMap/App/AppDelegate.swift
+++ StoryMap/StoryMap/App/AppDelegate.swift
@@ -6,10 +6,12 @@
 //
 
 import UIKit
+import OSLog
+
+public let logger = Logger()
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
           return true
     }
